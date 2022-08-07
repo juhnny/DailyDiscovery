86ccc1dad5573e02cd03ffe9baa31b5dc4d4a462
juhnny <iwibest@naver.com>
2022-08-07 21:14:05 +0900

    #5 build: fixed build-related problems
    build.gradle
    - debuggable true 추가 : 디버그 도구를 사용 가능하도록
    - ResponseHeader.kt, ResponseHeader.kt
      - @Keep annotation 추가 : minify 시 누락되는 클래스들. Retrofit이 파싱에 실패해 NPE 발생하였음
    - 프로젝트 폴더에 keystore.properties 생성(.gitignore에 추가), 키스토어 정보 저장, signingConfigs에서 참조. Git 공유 시 키스토어 정보 보안 목적

--- app/src/main/java/com/juhnny/dailydiscovery/Response.kt
+++ app/src/main/java/com/juhnny/dailydiscovery/Response.kt
@@ -1,8 +1,4 @@
 package com.juhnny.dailydiscovery
 
-data class Response<T>(val responseHeader: com.juhnny.dailydiscovery.ResponseHeader,
-                       val responseBody: com.juhnny.dailydiscovery.ResponseBody<T>) {
-
-
-
-}
+data class Response<T>(val responseHeader: ResponseHeader,
+                       val responseBody: ResponseBody<T>) {}
--- app/src/main/java/com/juhnny/dailydiscovery/ResponseBody.kt
+++ app/src/main/java/com/juhnny/dailydiscovery/ResponseBody.kt
@@ -1,5 +1,14 @@
 package com.juhnny.dailydiscovery
 
+import androidx.annotation.Keep
+
+// release build에서 Retrofit2가 json을 변환하지 못하는 문제 발생
+// 서버로부터 들어오는 데이터는 String으로 찍어보니 문제 없었음
+// build.grade(app)의 buildTypes의 release에서 minifyEnabled true를 해제하니 전환 성공
+// minify는 빌드 시 쓰이지 않는 클래스들을 제외시키는 것
+// 즉, 이 데이터클래스들이 쓰이지 않는다고 판단해서 빌드에 포함이 되지 않아 Retrofit2에서 파싱하지 못한 것
+// 클래스에 @Keep annotation을 사용하면 minify 시 무조건 빌드에 포함됨 -> 해결!
+@Keep
 data class ResponseBody<T> (val itemCount:Int,
                             val items:List<T>) {
 
